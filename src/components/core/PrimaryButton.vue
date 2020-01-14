ed245211d31cfb5bf902a40fd9bbcc7f9c21441e
Luiz Almeida <luizhrqas@gmail.com>
2020-01-14 18:53:12 -0300

    Add Hero Unit component for a better presentation

---
+++ src/components/core/PrimaryButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="text-lg"><slot></slot></button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/components/screens/Home.vue
+++ src/components/screens/Home.vue
@@ -1,16 +1,25 @@
 <template>
-  <main>
+  <main class="flex flex-col h-screen">
     <Navbar />
-    <div class="container mx-auto">
-      <p>Generate your ARK Wallet here!</p>
+    <div class="flex items-center flex-1 text-center">
+      <div class="container mx-auto">
+        <h2 class="text-gray-800 text-3xl font-bold">
+          Start using our ARK Wallet
+        </h2>
+        <p class="text-gray-800 text-md mt-2">
+          Generate Your Own Unique Passphrase and Address
+        </p>
+        <PrimaryButton>Create new wallet</PrimaryButton>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
 import Navbar from "../core/Navbar";
+import PrimaryButton from "../core/PrimaryButton";
 export default {
-  components: { Navbar }
+  components: { Navbar, PrimaryButton }
 };
 </script>
 
