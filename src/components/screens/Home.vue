d46a83f501c3eba527d8e8ffa28c15237486537d
Luiz Almeida <luizhrqas@gmail.com>
2020-01-14 18:08:17 -0300

    Add core/Navbar and screens/Home components

---
+++ src/components/screens/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <Navbar />
+    <div class="container mx-auto">
+      <p>Generate your ARK Wallet here!</p>
+    </div>
+  </main>
+</template>
+
+<script>
+import Navbar from "../core/Navbar";
+export default {
+  components: { Navbar }
+};
+</script>
+
+<style></style>
