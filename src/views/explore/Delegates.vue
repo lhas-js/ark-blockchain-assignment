028d696c6ec77d6f8bc8ed758bbf68b38bef84e4
Luiz Almeida <luizhrqas@gmail.com>
2020-01-20 04:39:56 -0300

    Add Delegate Monitor route

--- src/views/explore/Delegates.vue
+++ src/views/explore/Delegates.vue
@@ -1,7 +1,68 @@
-<template><div></div></template>
+<template>
+  <main class="flex flex-col h-screen text-gray-600">
+    <Navbar />
+    <div class="flex flex-1">
+      <div class="container mx-auto">
+        <h2 class="text-gray-800 text-2xl my-2 font-extrabold">
+          <router-link class="text-gray-500 rounded mr-2 inline-block" to="/explore">
+            <Back />
+          </router-link>Delegate Monitor
+        </h2>
 
-<script>
-export default {};
+        <div
+          class="text-gray-500 text-md bg-white rounded-lg shadow-md overflow-x-auto md:overflow-hidden my-3"
+        >
+          <table class="table-auto w-full">
+            <thead>
+              <tr>
+                <th class="text-gray-400 font-semibold px-4 py-4">Rank</th>
+                <th class="text-gray-400 font-semibold px-4 py-4">Username</th>
+                <th class="text-gray-400 font-semibold px-4 py-4">Votes</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-bind:key="delegate.id" v-for="(delegate, index) in formattedDelegates">
+                <td class="border px-4 py-2">{{index+1}}</td>
+                <td class="border px-4 py-2">{{delegate.username}}</td>
+                <td class="border px-4 py-2">{{delegate.humanizedVotes}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Navbar from "@/components/Navbar.vue";
+import Back from "@/components/icons/Back.vue";
+import WalletService from "@/services/WalletService";
+import { IWallet, IDelegate } from "@/interfaces";
+import { isWalletAddress, isPublicKey, readableCrypto } from "@/utils";
+import { getAddress } from "@/services/CryptoService";
+import ArkService from "../../services/ArkService";
+
+export default Vue.extend({
+  components: { Navbar, Back },
+  data: () => ({
+    page: 1,
+    delegates: [] as IDelegate[]
+  }),
+  mounted: async function() {
+    const request = await ArkService.getDelegates(1);
+
+    this.delegates = request.data.data;
+  },
+  computed: {
+    formattedDelegates() {
+      const response = this.delegates.map(delegate => ({
+        ...delegate,
+        humanizedVotes: readableCrypto(delegate.votes)
+      }));
+      return response;
+    }
+  }
+});
 </script>
-
-<style></style>
